66141a3fac2f18bd69da7ea343bd524097ea921a
OM PRAKASH DWIVEDI <40059915+omprakash-dwivedi28@users.noreply.github.com>
2024-06-25 14:05:19 +0530

    update video pages

--- src/Component/SummaryVideo.jsx
+++ src/Component/SummaryVideo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "../Component/css/summaryvideo.css"; // Import CSS file for styling
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
@@ -39,7 +39,7 @@ function SummaryVideo() {
         <table className="table table-bordered table-striped feedback-table">
           <thead className="thead-dark">
             <tr>
-              <th>Serial ID</th>
+              <th>Video ID</th>
               <th>Deptt</th>
               <th>Subject</th>
               <th>Topic</th>
--- src/Component/VideoLearner.jsx
+++ src/Component/VideoLearner.jsx
@@ -4,12 +4,15 @@ import axios from "axios";
 import "../Component/css/VideoLearner.css";
 import { useInitialContext } from "../context/InitialContext";
 import { VscLoading } from "react-icons/vsc";
+import { AiOutlineLike } from "react-icons/ai";
+import { IoSend } from "react-icons/io5";
+import { TbMessage2Down } from "react-icons/tb";
 
 const VideoLearner = () => {
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredVideos, setFilteredVideos] = useState([]);
   const [subjects, setSubjects] = useState([]);
   const [topcodes, setTopcodes] = useState([]);
@@ -17,10 +20,15 @@ const VideoLearner = () => {
   const [selectedTopcode, setSelectedTopcode] = useState("");
   const [searchFromDate, setSearchFromDate] = useState("");
   const [searchToDate, setSearchToDate] = useState("");
-
   const [currentPage, setCurrentPage] = useState(1);
+  const [videolikes, setVideolikes] = useState(null);
+  const [active, setActive] = useState(false);
   const videosPerPage = 10;
   const { adminloginData } = useInitialContext();
+  const [likedVideos, setLikedVideos] = useState(new Set());
+
+  const [comments, setComments] = useState({});
+  const [commentInput, setCommentInput] = useState({});
 
   useEffect(() => {
     fetch("https://railwaymcq.com/student/videolinks_api.php")
@@ -79,7 +87,7 @@ const VideoLearner = () => {
   };
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+    setSearchQuery(event.target.value);
   };
 
   const handleDateChange = (event, type) => {
@@ -94,7 +102,8 @@ const VideoLearner = () => {
   useEffect(() => {
     const filtered = videos.filter((video) => {
       return (
-        video.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (video.id.toString().includes(searchQuery) ||
+          video.title.toLowerCase().includes(searchQuery.toLowerCase())) &&
         (selectedSubject === "" ||
           video.subcode.toString() === selectedSubject) &&
         (selectedTopcode === "" ||
@@ -108,7 +117,7 @@ const VideoLearner = () => {
     setFilteredVideos(filtered);
     setCurrentPage(1);
   }, [
-    searchTerm,
+    searchQuery,
     videos,
     selectedSubject,
     selectedTopcode,
@@ -116,7 +125,6 @@ const VideoLearner = () => {
     searchToDate,
   ]);
 
-  // Pagination logic
   const indexOfLastVideo = currentPage * videosPerPage;
   const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
   const currentVideos = filteredVideos.slice(
@@ -138,18 +146,110 @@ const VideoLearner = () => {
     }
   };
 
+  const updateViews = async (videoId) => {
+    try {
+      const updateviewsflag = "1";
+      const response = await axios.post(
+        "https://railwaymcq.com/student/updatevideoviews.php",
+        { videoId, updateviewsflag }
+      );
+
+      console.log("Update views Response:", response.data);
+      if (response.data.success) {
+        setVideos((prevVideos) =>
+          prevVideos.map((video) =>
+            video.id === videoId ? { ...video, views: video.views + 1 } : video
+          )
+        );
+      }
+    } catch (error) {
+      console.error("Failed to update views", error);
+    }
+  };
+
+  const updateLikes = async (videoId) => {
+    try {
+      const updatelikeflag = "1";
+      const response = await axios.post(
+        "https://railwaymcq.com/student/updatevideoviews.php",
+        { videoId, updatelikeflag }
+      );
+      console.log("Update like Response:", response.data);
+      if (response.data.success) {
+        setLikedVideos(new Set(likedVideos.add(videoId)));
+        setVideos((prevVideos) =>
+          prevVideos.map((video) =>
+            video.id === videoId ? { ...video, likes: video.likes + 1 } : video
+          )
+        );
+      }
+    } catch (error) {
+      console.error("Failed to update likes", error);
+    }
+  };
+
+  const handlelikebtn = (video) => {
+    if (!likedVideos.has(video.id)) {
+      updateLikes(video.id);
+      setLikedVideos(new Set(likedVideos.add(video.id)));
+      setVideolikes(video.likes + 1);
+    }
+  };
+
+  const handleCommentChange = (videoId, event) => {
+    setCommentInput({ ...commentInput, [videoId]: event.target.value });
+  };
+
+  const handleCommentSubmit = async (videoId) => {
+    try {
+      const response = await axios.post(
+        "https://railwaymcq.com/student/videocomment_api.php",
+        { videoId, comment: commentInput[videoId] }
+      );
+
+      if (response.data.success) {
+        handleshowComment(videoId);
+        setComments({
+          ...comments,
+          [videoId]: [
+            ...(comments[videoId] || []),
+            { text: commentInput[videoId] },
+          ],
+        });
+        setCommentInput({ ...commentInput, [videoId]: "" });
+      }
+    } catch (error) {
+      console.error("Failed to submit comment", error);
+    }
+  };
+
+  const handleshowComment = async (videoId) => {
+    try {
+      const response = await axios.get(
+        `https://railwaymcq.com/student/videocomment_api.php?videoId=${videoId}`
+      );
+      console.log(response.data.comments);
+      setComments((prevComments) => ({
+        ...prevComments,
+        [videoId]: response.data.comments || [],
+      }));
+    } catch (error) {
+      console.error("Failed to fetch comments", error);
+    }
+  };
+
   return (
     <div className="video-learner-container">
       <div className="video-list">
-        <h4>Welcome!! {adminloginData?.userData?.name}</h4>{" "}
+        <h4>Welcome!! {adminloginData?.userData?.name}</h4>
       </div>
       <h2 className="video-learner-heading">Video Learning Playlist</h2>
       <p className="total-videos">Total Videos: {filteredVideos.length}</p>
       <div className="search-container">
         <input
           type="text"
-          placeholder="Search videos..."
-          value={searchTerm}
+          placeholder="Search by video ID or title..."
+          value={searchQuery}
           onChange={handleSearchChange}
           className="search-box"
         />
@@ -209,16 +309,75 @@ const VideoLearner = () => {
         <div className="video-list">
           {currentVideos.map((video) => (
             <div key={video.id} className="video-item">
-              <div
-                className="video-wrapper"
-                onClick={() => handleVideoSelect(video)}
-              >
+              <div className="video-wrapper">
+                <div className="video-id">VID: {video.id}</div>
                 <YouTube
                   videoId={video.link}
+                  onPlay={() => updateViews(video.id)}
                   className="video-player"
                   opts={{ width: "100%", height: "200px" }}
                 />
-                <h3 className="video-title">{video.title}</h3>
+                <div className="video-info">
+                  <h3 className="video-title">{video.title}</h3>
+                  <div className="video-stats">
+                    <p className="views-count">Views: {video.views}</p>
+                    <div>
+                      <div
+                        type="button"
+                        className={`like-button ${
+                          likedVideos.has(video.id) ? "liked" : ""
+                        }`}
+                        onClick={() => handlelikebtn(video)}
+                        disabled={active || likedVideos.has(video.id)}
+                      >
+                        <AiOutlineLike
+                          fontSize={30}
+                          color={likedVideos.has(video.id) ? "green" : "red"}
+                        />
+                      </div>
+                      <span className="like-count">{video.likes}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="comment-section1">
+                  <input
+                    type="text"
+                    placeholder="Add a comment..."
+                    value={commentInput[video.id] || ""}
+                    onChange={(e) => handleCommentChange(video.id, e)}
+                    className="comment-box"
+                  />
+                  <IoSend
+                    fontSize={60}
+                    color="green"
+                    onClick={() => handleCommentSubmit(video.id)}
+                    className="send-icon"
+                  />
+                </div>
+                <div className="comment-section">
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <TbMessage2Down
+                      fontSize={30}
+                      color="green"
+                      onClick={() => handleshowComment(video.id)}
+                    />
+                  </div>
+                  <div className="comments-list">
+                    {comments[video.id] && comments[video.id].length > 0 ? (
+                      comments[video.id].map((comment, index) => (
+                        <p key={index} className="comment">
+                          {/* {comment.comment}
+                           */}
+                          {comment.comment ? comment.comment : comment.text}
+                          {/* {console.log(comment)} */}
+                        </p>
+                      ))
+                    ) : (
+                      <p></p>
+                      // <p>No comments yet</p>
+                    )}
+                  </div>
+                </div>
               </div>
               <a
                 href={`https://www.youtube.com/watch?v=${video.link}`}
